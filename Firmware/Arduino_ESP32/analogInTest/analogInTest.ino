22a9ef8c8e79dc5ead7be5a3679dc21c481dddbe
Iván Morales <ivan.rene.morales@gmail.com>
2020-03-12 19:55:48 -0600

    Initial PWM + ADC test successfully done
    Using 4v5 as power supply plugged into ESP32's VIN input.

--- Firmware/Arduino_ESP32/analogInTest/analogInTest.ino
+++ Firmware/Arduino_ESP32/analogInTest/analogInTest.ino
@@ -1,13 +1,24 @@
-#define PIN_PWM 17 //GPIO17
+#define L1 25 //3v3 Output
+#define L0 26 //0V Output
+
+#define PIN_PWM 27 //GPIO35
 #define PIN_POT 34 //ADC6
 
 #define PWM_CHAN 0 //PWM Channel (FreeRTOS Timer - based)
 #define PWM_FREQ 5000 // PWM Frequency (Hz)
 #define PWM_RES 12 //12 BIT PWM OUTPUT
+#define MAX_PWM 4096 //12 BIT PWM OUTPUT
 
-unsigned int potIn;
+unsigned long potIn;
 
 void setup() {
+  pinMode(L1, OUTPUT);
+  digitalWrite(L1, 1);
+  
+  pinMode(L0, OUTPUT);
+  pinMode(L0, 0);
+  
+  
   pinMode(PIN_PWM, OUTPUT);
   digitalWrite(PIN_PWM, 0);
   ledcSetup(PWM_CHAN, PWM_FREQ, PWM_RES);
@@ -15,17 +26,31 @@ void setup() {
 
   pinMode(PIN_POT, INPUT);
 
+  Serial.begin(115200);
+
+  Serial.println("Initialization complete!");
+
 }
 
 void loop() {
   int i;
-  int n = 100;
+  int n = 10;
   for(i = 0; i < n; i++){
-    potIn += (analogRead(potIn));
+    potIn += (analogRead(PIN_POT));
+    //Serial.print("Analog read: ");
+    //Serial.println(analogRead(PIN_POT));
     delay(1);
   }
   potIn /= n;
 
-  ledcWrite(PWM_CHAN, potIn);
+  potIn = (potIn > MAX_PWM)?MAX_PWM:potIn;
+
+  ledcWrite(PWM_CHAN, MAX_PWM-potIn);
+
+  //Serial.print("New PWM set: ");
+  //Serial.println(MAX_PWM-potIn);
+
+  delay(1);
+
 
 }
